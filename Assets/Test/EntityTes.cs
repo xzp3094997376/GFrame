34323b883df6520dd6b7b5b70bd2d37180b3d8d1
xzp <3094997376@qq.com>
2022-04-09 09:24:42 +0800

    init

---
+++ Assets/Test/EntityTes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+public class EntityTes : EntityLogic
+{
+    // Start is called before the first frame update
+    protected override void OnShow(object userData)
+    {
+        base.OnShow(userData);
+        Log.Debug("cube loaded");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
